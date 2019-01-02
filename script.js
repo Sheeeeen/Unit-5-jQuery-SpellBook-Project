5c9ea8ec82fe1046d0758be08b19ccf66365f638
Sheeeeen <43342103+Sheeeeen@users.noreply.github.com>
2019-01-02 08:29:58 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#disappear").click(function(){
+ $("#napkin").hide();
+ $("#dove").show();
+});
+
+
+
+//Spell #2
+
+$("#eat").click(function(){
+ $("#razor").slideUp();
+});
+
+
+
+//Spell #3
+
+$("#snap").click(function(){
+ $("#spiderman").fadeOut();
+});
+
+
+
